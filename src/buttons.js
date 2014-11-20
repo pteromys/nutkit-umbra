817a51d330ac6795b5fdc3e4b7d3373a009f162e
pteromys <pteromys@gmail.com>
2014-11-20 13:05:58 -0600

    buttons: restore automatic activation.
    (Broken in the move to browserify.)
    
    Also, buttons is more like a namespace than an object;
    so reformat the definition to reflect that.

--- src/buttons.js
+++ src/buttons.js
@@ -1,7 +1,7 @@
 var KEYS = require('./keys.js');
 var $ = require('jquery');
 
-var clickElement = function (elt) {
+function clickElement(elt) {
 	// jQuery click() won't open hyperlinks, so we do it ourselves.
 	elt = $(elt);
 	var ev = $.Event("click");
@@ -9,60 +9,64 @@ var clickElement = function (elt) {
 	if (elt.is('a') && !ev.isDefaultPrevented()) {
 		window.location = elt.attr('href');
 	}
+}
+
+function activate() {
+	$(document).ready(activateRootButtons);
+	$(document).ready(activateClickouts);
+}
+
+function activateRootButtons() {
+	$('.button_root').on('click', function (e) {
+		e.stopPropagation();
+		if (window.location.hash == $(this).attr('href')) {
+			e.preventDefault();
+			window.location = '#';
+		}
+	});
+	var bindings = {};
+	$('.button_root').each(function () {
+		var k = $(this).data('key');
+		if (!k) { return; }
+		if (k in KEYS) { k = KEYS[k]; }
+		bindings[k] = $(this);
+	});
+	activateKeys(bindings);
+}
+
+function activateKeys(bindings) {
+	bindings = bindings || {};
+	$(window).on('keydown', function (e) {
+		if ($('.overlay').is(':target')) {
+			if (e.which == KEYS.ESC) {
+				var target = $(e.target);
+				var target2 = target.find(':target');
+				if (target2.length) { target = target2; }
+				window.setTimeout(function () {
+					clickElement(target.find('.button_close'));
+				}, 10);
+			}
+		} else {
+			if (bindings[e.which]) {
+				clickElement($(bindings[e.which]));
+			}
+		}
+	});
+}
+
+function activateClickouts() {
+	$('body').on('click.clickout', '.overlay', function (e) {
+		if (e.target == this) { // i.e. if not bubbled
+			clickElement($(e.target).find('.button_close'));
+		}
+	});
+}
+
+module.exports = {
+	"activate": activate,
+	"activateRootButtons": activateRootButtons,
+	"activateKeys": activateKeys,
+	"activateClickouts": activateClickouts,
 };
-
-var ButtonSystem = {
-	activate: function () {
-		$(document).ready(this.activateRootButtons);
-		$(document).ready(this.activateClickouts);
-	},
-	activateRootButtons: function () {
-		$('.button_root').on('click', function (e) {
-			e.stopPropagation();
-			if (window.location.hash == $(this).attr('href')) {
-				e.preventDefault();
-				window.location = '#';
-			}
-		});
-		var bindings = {};
-		$('.button_root').each(function () {
-			var k = $(this).data('key');
-			if (!k) { return; }
-			if (k in KEYS) { k = KEYS[k]; }
-			bindings[k] = $(this);
-		});
-		this.activateKeys(bindings);
-	},
-	activateKeys: function (bindings) {
-		bindings = bindings || {};
-		$(window).on('keydown', function (e) {
-			if ($('.overlay').is(':target')) {
-				if (e.which == KEYS.ESC) {
-					var target = $(e.target);
-					var target2 = target.find(':target');
-					if (target2.length) { target = target2; }
-					window.setTimeout(function () {
-						clickElement(target.find('.button_close'));
-					}, 10);
-				}
-			} else {
-				if (bindings[e.which]) {
-					clickElement($(bindings[e.which]));
-				}
-			}
-		});
-	},
-	activateClickouts: function () {
-		$('body').on('click.clickout', '.overlay', function (e) {
-			if (e.target == this) { // i.e. if not bubbled
-				clickElement($(e.target).find('.button_close'));
-			}
-		});
-	},
-};
-
-ButtonSystem.activateRootButtons = ButtonSystem.activateRootButtons.bind(ButtonSystem);
-
-module.exports = ButtonSystem;
 
 
--- src/umbra.js
+++ src/umbra.js
@@ -1,6 +1,10 @@
+var $ = require('jquery');
+var buttons = require('./buttons.js');
+if (document) { buttons.activate(); }
+
 module.exports = {
 	"KEYS": require('./keys.js'),
-	"button_system": require('./buttons.js'),
+	"button_system": buttons,
 	"Form": require('./forms.js'),
 	"MessageBox": require('./messagebox.js'),
 };
